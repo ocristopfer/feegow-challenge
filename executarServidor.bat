e5e921c4c424af36b9c0b15609c0bab7a0d43f86
Cristopfer Luis <ocristopfer@gmail.com>
2020-10-17 02:55:59 -0300

    -Correção no Servidor Web PHP embutido, não estava carregando as extensões corretamente.

--- executarServidor.bat
+++ executarServidor.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-cd src
 start chrome --disable-web-security  --user-data-dir=%temp%\chromeTemp http://localhost:8000/ui/agendamento/
-start cmd /C ..\php\php.exe -S localhost:8000
+start cmd /C php\php.exe -S localhost:8000 -c php\php.ini -t src
+REM start cmd /C php\php.exe -S localhost:8000 -c php.ini -t \src\
 cd ..
